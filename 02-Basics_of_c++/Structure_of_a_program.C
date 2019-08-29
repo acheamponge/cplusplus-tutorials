58c6b01172a6bcd5c3d53e5aa08577f28aade96e
acheamponge <acheamponge@berea.edu>
2019-08-29 01:56:50 +0000

    completed 2nd lesson

---
+++ 02-Basics_of_c++/Structure_of_a_program.C
@@ -0,0 +1,10 @@
+//First program in c++
+
+#include <iostream> //directive interpreted by preprocessor to include iostream header.
+
+int main() //declares main function which always returns an int.
+{ 
+	std::cout << "Hello World! " << std::endl; //prints Hello World
+}
+
+//To stick to a strict standard, I will not be using namespace std; in all the code.
